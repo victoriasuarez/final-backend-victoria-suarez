e430c39758dd9be1af8f06ad0596c2d714e5b2eb
victoriasuarez <victoriasuarez49@gmail.com>
2022-12-12 17:17:21 -0300

    Actualización.

--- catalog-service/src/test/java/com/finalback/victoriasuarez/catalogservice/BaseAPI.java
+++ catalog-service/src/test/java/com/finalback/victoriasuarez/catalogservice/BaseAPI.java
@@ -1,34 +1,34 @@
-//package com.finalback.victoriasuarez.catalogservice;
-//
-//import io.restassured.RestAssured;
-//import io.restassured.config.SSLConfig;
-//import io.restassured.path.json.config.JsonPathConfig;
-//import org.junit.jupiter.api.BeforeAll;
-//
-//import static io.restassured.RestAssured.*;
-//import static io.restassured.config.JsonConfig.jsonConfig;
-//import static io.restassured.config.RestAssuredConfig.newConfig;
-//
-//public class BaseAPI {
-//
-//    protected static Configuration configuration;
-//
-//    @BeforeAll
-//    public static void beforeAllTests() {
-//        configuration = ConfigurationManager.getConfiguration();
-//
-//        baseURI = configuration.baseURI();
-//        basePath = configuration.basePath();
-//        port = configuration.port();
-//
-//        // solve the problem with big decimal assertions
-//        config = newConfig().
-//                jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL)).
-//                sslConfig(new SSLConfig().allowAllHostnames());
-//
-//        RestAssured.useRelaxedHTTPSValidation();
-//        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
-//
-//    }
-//
-//}
+package com.finalback.victoriasuarez.catalogservice;
+
+import io.restassured.RestAssured;
+import io.restassured.config.SSLConfig;
+import io.restassured.path.json.config.JsonPathConfig;
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.*;
+import static io.restassured.config.JsonConfig.jsonConfig;
+import static io.restassured.config.RestAssuredConfig.newConfig;
+
+public class BaseAPI {
+
+    protected static Configuration configuration;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        configuration = ConfigurationManager.getConfiguration();
+
+        baseURI = configuration.baseURI();
+        basePath = configuration.basePath();
+        port = configuration.port();
+
+        // solve the problem with big decimal assertions
+        config = newConfig().
+                jsonConfig(jsonConfig().numberReturnType(JsonPathConfig.NumberReturnType.BIG_DECIMAL)).
+                sslConfig(new SSLConfig().allowAllHostnames());
+
+        RestAssured.useRelaxedHTTPSValidation();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    }
+
+}
--- catalog-service/src/test/java/com/finalback/victoriasuarez/catalogservice/CatalogTest.java
+++ catalog-service/src/test/java/com/finalback/victoriasuarez/catalogservice/CatalogTest.java
@@ -1,75 +1,75 @@
-//package com.finalback.victoriasuarez.catalogservice;
-//
-//import com.finalback.victoriasuarez.catalogservice.model.Catalog;
-//import io.restassured.http.ContentType;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Tag;
-//import org.junit.jupiter.api.Test;
-//
-//import static io.restassured.RestAssured.given;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//class CatalogTest extends BaseAPI {
-//
-//    @Test
-//    @Tag("Functional")
-//    @DisplayName("Testear toda la aplicación con api gateway")
-//    void testingAll() {
-//
-//        given().
-//                contentType(ContentType.JSON).
-//                body(
-//                        new MovieRequest(198L, "movie new", "genre", "url")
-//                ).
-//                when().post("/movies");
-//
-//        given().
-//                contentType(ContentType.JSON).
-//                body(
-//                        new SerieRequest(945L, "serie new", "genre")
-//                ).
-//                when().post("/series");
-//
-//        Catalog responseOnline = given()
-//                .pathParams("genre", "genre")
-//                .when().get("/catalogs/online/{genre}")
-//                .as(Catalog.class);
-//
-//        Catalog responseOffline = given()
-//                .pathParams("genre", "genre")
-//                .when().get("/catalogs/offline/{genre}")
-//                .as(Catalog.class);
-//
-//        assertEquals(responseOnline.getMovies().size(), 1);
-//        assertEquals(responseOnline.getSeries().size(), 1);
-//
-//        assertEquals(responseOffline.getMovies().size(), 1);
-//        assertEquals(responseOffline.getSeries().size(), 1);
-//    }
-//
-//    @Getter
-//    @Setter
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    public static class MovieRequest {
-//        private Long id;
-//        private String name;
-//        private String genre;
-//        private String urlStream;
-//    }
-//
-//    @Getter
-//    @Setter
-//    @NoArgsConstructor
-//    @AllArgsConstructor
-//    public static class SerieRequest {
-//        private Long id;
-//        private String name;
-//        private String genre;
-//    }
-//
-//}
+package com.finalback.victoriasuarez.catalogservice;
+
+import com.finalback.victoriasuarez.catalogservice.model.Catalog;
+import io.restassured.http.ContentType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CatalogTest extends BaseAPI {
+
+    @Test
+    @Tag("Functional")
+    @DisplayName("Testear toda la aplicación con api gateway")
+    void testingAll() {
+
+        given().
+                contentType(ContentType.JSON).
+                body(
+                        new MovieRequest(198L, "Sinister", "Suspense", "url")
+                ).
+                when().post("/movies/save");
+
+        given().
+                contentType(ContentType.JSON).
+                body(
+                        new SerieRequest(945L, "Manifiesto", "Suspense")
+                ).
+                when().post("/series/save");
+
+        Catalog responseOnline = given()
+                .pathParams("genre", "Suspense")
+                .when().get("/catalogs/online/{genre}")
+                .andReturn().body().as(Catalog.class);
+
+        Catalog responseOffline = given()
+                .pathParams("genre", "Manifiesto")
+                .when().get("/catalogs/offline/{genre}")
+                .andReturn().body().as(Catalog.class);
+
+        assertEquals(responseOnline.getMovies().size(), 1);
+        assertEquals(responseOnline.getSeries().size(), 1);
+
+        assertEquals(responseOffline.getMovies().size(), 1);
+        assertEquals(responseOffline.getSeries().size(), 1);
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MovieRequest {
+        private Long id;
+        private String name;
+        private String genre;
+        private String urlStream;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class SerieRequest {
+        private Long id;
+        private String name;
+        private String genre;
+    }
+
+}
